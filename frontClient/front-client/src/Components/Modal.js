71ccc2f036f3e5cf8f8b70a7e00af35ab29974cf
AnnaKuzmich1991 <kuzmichanna1.22@gmail.com>
2024-02-22 23:45:30 +0300

    Modal

--- frontClient/front-client/src/Components/Modal.js
+++ frontClient/front-client/src/Components/Modal.js
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
 import { Modal, Button, Form } from 'react-bootstrap';
 import axios from 'axios';
+import { useParams } from "react-router-dom";
 
 function CustomModal({ show, onHide }) {
+    const { id } = useParams(); // Используйте id при необходимости для запросов к API
     const [formData, setFormData] = useState({
         phone: '',
         full_name: '',
         doctor_name: '',
         message: '',
+        user_id: id // Добавлено user_id в состояние формы
     });
 
     const handleInputChange = (e) => {
@@ -18,28 +21,29 @@ function CustomModal({ show, onHide }) {
         }));
     };
 
+    // Убедитесь, что user_id обновляется, если id изменится во время жизненного цикла компонента
+    // Это может быть полезно, если компонент используется на странице, где id может изменяться без перезагрузки компонента
+    React.useEffect(() => {
+        setFormData(formData => ({ ...formData, user_id: id }));
+    }, [id]);
+
     const sendDataToServer = () => {
-        const token = localStorage.getItem("token");
-
-        if (!token) {
-            console.error("Token not found");
-            return;
-        }
-
-        axios.post("http://localhost:8080/api/v1/your-endpoint", formData, {
+        const apiUrl = `http://your-api-endpoint.com/api/path`; // Замените на ваш фактический API endpoint
+        axios.post(apiUrl, formData, {
             headers: {
-                'Authorization': `Bearer ${token}`,
-                'Content-Type': 'application/json'
+                'Authorization': `Bearer ${localStorage.getItem("token")}` // Если требуется авторизация
             }
         })
             .then(response => {
-                console.log('Data sent successfully', response.data);
-                onHide(); // Закрыть модальное окно при успешной отправке
+                console.log(response.data);
+                onHide(); // Закрыть модальное окно при успехе
             })
             .catch(error => {
-                console.error('Error sending data', error);
+                console.error("There was an error!", error);
             });
     };
+
+
 
     return (
         <Modal show={show} onHide={onHide}>
